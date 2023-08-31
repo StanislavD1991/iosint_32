81ff9324d56d0aacb0340171b7683eb7bc91a217
Stanislav <mr.dorrin91@gmail.com>
2023-08-31 19:19:16 +0300

    iosint-homeworks_2

---
+++ iosint-32/Navigation/Navigation/Constans/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  Navigator
+//
+//  Created by Stanislav on 16.08.2023.
+//
+
+import Foundation
+
+// MARK: - Post
+
+struct Post {
+    var author: String
+    var description: String
+    var image: String
+    var likes: Int
+    var views: Int
+}
